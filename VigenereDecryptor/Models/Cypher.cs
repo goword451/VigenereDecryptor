43b3ae8445ad6d468fb922568b16b22a7d47f1b0
Anton Pavlov <goword45@gmail.com>
2021-04-27 06:53:17 +0300

    Добавьте файлы проекта.

---
+++ VigenereDecryptor/Models/Cypher.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace VigenereDecryptor.Models
+{
+    public class Cypher
+    {
+        private static readonly string alphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+        
+        public string Encrypt(string text, string key)
+        {
+            int i = 0;
+            string result = "";
+            foreach (char sym in text)
+            {
+                char lowerSym = char.ToLower(sym);
+                if (alphabet.Contains(lowerSym))
+                {
+                    int p = alphabet.IndexOf(lowerSym);
+                    int k = alphabet.IndexOf(char.ToLower(key[i++ % key.Length]));
+                    result += char.IsUpper(sym) ? alphabet[(p + k) % alphabet.Length].ToString().ToUpper()
+                                                : alphabet[(p + k) % alphabet.Length].ToString();
+                }
+                else
+                    result += sym;
+            }
+            return result;
+        }
+        
+        public string Decrypt(string text, string key)
+        {
+            int i = 0;
+            string result = "";
+            foreach (char sym in text)
+            {
+                char lowerSym = char.ToLower(sym);
+                if (alphabet.Contains(lowerSym))
+                {
+                    int c = alphabet.IndexOf(lowerSym);
+                    int k = alphabet.IndexOf(char.ToLower(key[i++ % key.Length]));
+                    result += char.IsUpper(sym) ? alphabet[(c - k + alphabet.Length) % alphabet.Length].ToString().ToUpper()
+                                                : alphabet[(c - k + alphabet.Length) % alphabet.Length].ToString();
+                }
+                else
+                    result += sym;
+            }
+            return result;
+        }
+    }
+}
---
+++ VigenereDecryptor/Pages/Index.cshtml.cs
@@ -0,0 +1,127 @@
+﻿using DocumentFormat.OpenXml.Packaging;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System.IO;
+using System.Text;
+using VigenereDecryptor.Models;
+
+namespace VigenereDecryptor.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly IWebHostEnvironment webHostEnvironment;
+        private readonly ILogger<IndexModel> _logger;
+
+        [BindProperty]
+        public string Input { get; set; }
+        public string Keyword { get; set; }
+        public string Output { get; set; }
+        [BindProperty]
+        public IFormFile InputFile { get; set; }
+        [BindProperty]
+        public string SelectedAlph { get; set; }
+
+
+        public IndexModel(ILogger<IndexModel> logger,
+            IWebHostEnvironment webHostEnvironment)
+        {
+            _logger = logger;
+            this.webHostEnvironment = webHostEnvironment;
+        }
+
+        public void OnPostAsync(string input, string keyword, string radio)
+        {
+            if (InputFile != null)
+            {
+                FileParser();
+            }
+            else
+            {
+                Input = input;
+            }
+
+            var encryptor = new Cypher();
+
+            Keyword = keyword;
+
+            if (string.IsNullOrEmpty(Input) || string.IsNullOrEmpty(Keyword))
+            {
+                return;
+            }
+
+            try
+            {
+                Output = radio == "encrypt" ?
+                    encryptor.Encrypt(Input, Keyword) : encryptor.Decrypt(Input, Keyword);
+            }
+            catch
+            {
+                _logger.LogError("Используйте русский алфавит");
+                Output = Input;
+            }
+
+            GenerateFiles();
+        }
+
+        private void GenerateFiles()
+        {
+            var uploadFolder = Path.Combine(webHostEnvironment.WebRootPath,
+                    "Files");
+            var filePathTxt = Path.Combine(uploadFolder, "output.txt");
+            var filePathDocx = Path.Combine(uploadFolder, "output.docx");
+
+            using (var fs = new FileStream(filePathTxt, FileMode.Create))
+            {
+                var array = Encoding.Default.GetBytes(Output);
+                fs.Write(array, 0, array.Length);
+            }
+
+            using (var fs = new FileStream(filePathDocx, FileMode.Create))
+            {
+                var array = Encoding.Default.GetBytes(Output);
+                fs.Write(array, 0, array.Length);
+            }
+
+        }
+
+        private void FileParser()
+        {
+            var extension = InputFile.FileName.Split('.')[1];
+            var uploadFolder = Path.Combine(webHostEnvironment.WebRootPath,
+                    "Files");
+            var filePath = Path.Combine(uploadFolder, "input." + extension);
+
+            using (var fs = new FileStream(filePath, FileMode.Create))
+            {
+                InputFile.CopyTo(fs);
+            }
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+            if (extension == "txt")
+            {
+                Input = System.IO.File.ReadAllText(filePath);
+                if (Input.Contains('�'))
+                {
+                    Input = System.IO.File.ReadAllText(filePath, Encoding.GetEncoding(1251));
+                }
+            }
+            else
+            {
+                try
+                {
+                    using (var document = WordprocessingDocument.Open(filePath, false))
+                    {
+                        Input = document.MainDocumentPart.Document.Body.InnerText;
+                    }
+                }
+                catch
+                {
+                    _logger.LogError("Ошибка ввода docx");
+                }
+            }
+        }
+    }
+}
